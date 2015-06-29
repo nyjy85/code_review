01ac321deaf13d12cd601016f2ee7aae79c64217
Eunice Lee <eueu.lee@gmail.com>
2015-06-29 17:35:42 -0400

    added home controler and authentication

--- extension/js/application/states/home/home.js
+++ extension/js/application/states/home/home.js
@@ -3,16 +3,29 @@ app.config(function ($stateProvider) {
 	$stateProvider.state('home', {
 		url: '/home',
 		templateUrl: 'js/application/states/home/home.html',
-		controller: 'HomeCtrl'
+		controller: 'HomeCtrl',
+		resolve: {
+			Authenticate: function($http, $state) {
+				$http.get("http://localhost:1337/session").then(function(res) {
+					if (res.data) {
+						return
+					}
+					else {
+						$state.go('login')
+					}
+				});
+			}
+		}
 	});
 });
 
 //add Factory
-app.controller('HomeCtrl', function ($scope) {
+app.controller('HomeCtrl', function ($scope, $state, popupGitFactory) {
 
-  //isAuthenticated.
-
-  // $scope.displayName =
+  popupGitFactory.getUserInfo().then(function(user) {
+		$scope.displayName = user.user.github.name;
+		console.log(user.user)
+	})
 
   // UserFactory.getTotalPoints().then(function(points){
 	// 	$scope.manyPoints = points
