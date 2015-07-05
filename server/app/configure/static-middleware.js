26979cd6dd63133da8305c8df57f4b88f34ea88a
calvinnhobbes <cschoi3@gmail.com>
2015-07-05 16:12:34 -0400

    Got basic page up

--- server/app/configure/static-middleware.js
+++ server/app/configure/static-middleware.js
@@ -11,11 +11,15 @@ module.exports = function (app) {
     var npmPath = path.join(root, './node_modules');
     var publicPath = path.join(root, './public');
     var browserPath = path.join(root, './browser');
+    var bowerPath = path.join(root, './bower_components');
+    var iconsPath = path.join(root, './img');
 
     app.use(favicon(app.getValue('faviconPath')));
     app.use(express.static(uiBootstrapPath));
     app.use(express.static(npmPath));
     app.use(express.static(publicPath));
     app.use(express.static(browserPath));
+    app.use(express.static(bowerPath));
+    app.use(express.static(iconsPath));
 
 };
