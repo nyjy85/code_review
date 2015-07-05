9dea7ff891875f8ceeeb0f7ab18c4966e601d8a1
calvinnhobbes <cschoi3@gmail.com>
2015-07-05 17:28:46 -0400

    Saving landing page

---
+++ browser/js/common/factories/LandingPageFactory.js
@@ -0,0 +1,47 @@
+app.factory('LandingPageFactory', function($http) {
+    var domain = "http://localhost:1337";
+
+    return {
+
+        getUserInfo: function() {
+          return $http.get(domain + "/session").then(function(res){
+            return res.data; //res.data.user
+          });
+        },
+
+        getReposToAdd: function(token) {
+          console.log('hit getrepo', token)
+          return $http.get(domain + "/api/git", {params: token})
+          .then(function(res) {
+            return res.data;
+          })
+        },
+
+        //adding or deleting repo from profile
+        editRepo: function(user) {
+          console.log('addRepoToProfile factory',user.repos)
+          var repo = {repo: user.repos}
+          return $http.put(domain + "/api/users/" + user.github.username + "/editRepo", repo)
+          .then(function(res) {
+            return res.data;
+          })
+        },
+
+        listFiles: function(repo) {
+          console.log('list file names under the repo', repo)
+          return $http.get(domain + "/api/file/repo/" + repo.name)
+          .then(function(res) {
+            return res.data;
+          })
+        },
+
+        logout: function() {
+          console.log('hitting the factory')
+          return $http.get(domain +'/logout').then(function () {
+      			//this needs a logic remap
+            	console.log('logged out');
+      		});
+        }
+
+    }
+});
--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,32 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
+        templateUrl: 'js/home/home.html',
+        controller: 'LandingPageCtrl'
     });
+});
+
+app.controller('LandingPageCtrl', function ($scope, AuthService, $state, LandingPageFactory) {
+
+    $scope.login = {};
+    $scope.error = null;
+
+    $scope.userInfo = function(user){
+    	LandingPageFactory.getUserInfo().then(function(response){
+    		console.log("this is in the LANDING controller", response)
+    	})
+    }
+
+    $scope.sendLogin = function (loginInfo) {
+
+        $scope.error = null;
+
+        AuthService.login(loginInfo).then(function () {
+            $state.go('home');
+        }).catch(function () {
+            $scope.error = 'Invalid login credentials.';
+        });
+
+    };
+
 });
