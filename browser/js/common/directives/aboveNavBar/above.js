3e77130e4ff6b700e6b06e4f8cfc8d7ba8d25ece
Eunice Lee <eueu.lee@gmail.com>
2015-07-10 14:12:34 -0400

    navbar done

--- browser/js/common/directives/aboveNavBar/above.js
+++ browser/js/common/directives/aboveNavBar/above.js
@@ -4,7 +4,8 @@ app.directive('above', function(AuthService, $rootScope, AUTH_EVENTS, $state){
 		restrict: 'E',
 		scope: {},
 		templateUrl: 'js/common/directives/aboveNavBar/above.html',
-        controller: 'LoginCtrl',
+    controller: 'LoginCtrl',
+		//------------
 		link: function(scope) {
 			scope.user = null;
 
@@ -24,11 +25,45 @@ app.directive('above', function(AuthService, $rootScope, AUTH_EVENTS, $state){
 
             setUser();
 
+						//------------
+						scope.notifications = function () {
+
+								//------------
+								scope.user.notifications.map(function(n){
+									console.log('notificatiosn!!!!!')
+									var fileUrl = n.fileUrl;
+									var url = fileUrl.split('/');
+									n.repoName = url[4];
+									n.file = url[url.length-2] + '/' + url[url.length-1];
+									n.timestamp = Math.floor((Date.now() - Date.parse(n.timestamp))/1000/60) + 'min ago';
+									if(n.line) n.line = n.line.slice(2);
+
+									var message = [
+										{update: 'newHighlight', display: ' added ' + n.number + ' new comments on '+ n.repoName + "(" + n.file + ")      "},
+										{update: 'newComment', display: ' added '+ n.number + ' responses on ' + n.repoName + "(" + n.file +")"+' on line '+ n.line + "    "}
+									]
+
+									//------------
+									message.forEach(function(msg) {
+										if (n.update === msg.update) {
+											n.display = msg.display;
+										}
+									})
+									//------------
+
+								})
+								//------------
+								scope.noti = scope.user.notifications;
+						}
+						//------------
+					}
+//------------
+		}
             $rootScope.$on(AUTH_EVENTS.loginSuccess, setUser);
             $rootScope.$on(AUTH_EVENTS.logoutSuccess, removeUser);
             $rootScope.$on(AUTH_EVENTS.sessionTimeout, removeUser);
+		// }
+	})
 
-		}
-
-	};
-});
+// 	};
+// });
